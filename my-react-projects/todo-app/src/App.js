5a6b4fa86dd13d0b9e333c4596c67ca830b951f6
Deepak Chahal <chaliadeepakdaha@gmail.com>
2025-08-05 22:39:48 +0530

    Done

--- my-react-projects/todo-app/src/App.js
+++ my-react-projects/todo-app/src/App.js
@@ -1,21 +1,48 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Add } from "./Add";
 import { Show } from "./Show";
 import { Header } from "./Header";
-import { Completed } from './Completed.js';
+import { Done } from './Done.js';
 import { Footer } from './Footer'
 
 function App() {
+  let[todo,setTodo] = useState([
+    {
+      "id" :1,
+   "todoTitle": "Buy Clothes ",
+    "dueDate" : "2-08-25",
+   " todoStatus" : "Pending"
+    },
+        {
+      "id" :2,
+   "todoTitle": "Go to Gym",
+    "dueDate" : "4-08-25",
+   " todoStatus" : "Pending"
+    },
+        {
+      "id" :3,
+   "todoTitle": "Dancing",
+    "dueDate" : "12-08-25",
+   " todoStatus" : "Pending"
+    },
+        {
+      "id" :4,
+   "todoTitle": "Singing",
+    "dueDate" : "2-08-25",
+   " todoStatus" : "Pending"
+    },
+
+  ])
 
   return (
     <div class="flex flex-col min-h-screen justify-between">
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/Add" element={<Add />} />
-          <Route path="/Show" element={<Show />} />
-          <Route path="/Done" element={<Completed />} />
+          <Route path="/Add" element={<Add /> } />
+          <Route path="/Show" element={<Show todo={todo} setTodo={setTodo}/>} />
+          <Route path="/Done" element={<Done />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- my-react-projects/todo-app/src/Done.js
+++ my-react-projects/todo-app/src/Done.js
@@ -1,4 +1,4 @@
-function Completed() {
+function Done() {
   return (
     <div className="max-w-4xl mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
       <h2 className="text-2xl font-semibold mb-6 text-center text-black">Completed Tasks</h2>
@@ -6,25 +6,25 @@ function Completed() {
         <thead className="bg-gray-200">
           <tr>
             <th className="px-4 py-2 border">Todo Title</th>
-            <th className="px-4 py-2 border">Due Date</th>
-            <th className="px-4 py-2 border">Status</th>
+            <th className="px-4 py-2 border">Completed on</th>
+          
           </tr>
         </thead>
         <tbody>
           <tr className="text-center">
-            <td className="border px-4 py-2">Gaming</td>
+            <td className="border px-4 py-2">Go To Gym</td>
             <td className="border px-4 py-2">25-08-25</td>
-            <td className="border px-4 py-2">Completed</td>
+           
           </tr>
           <tr className="text-center">
-            <td className="border px-4 py-2">Internship</td>
+            <td className="border px-4 py-2">Singing</td>
             <td className="border px-4 py-2">2-08-25</td>
-            <td className="border px-4 py-2">Completed</td>
+           
           </tr>
           <tr className="text-center">
-            <td className="border px-4 py-2">Football</td>
+            <td className="border px-4 py-2">Dancing</td>
             <td className="border px-4 py-2">5-08-25</td>
-            <td className="border px-4 py-2">Completed</td>
+         
           </tr>
         </tbody>
       </table>
@@ -32,4 +32,4 @@ function Completed() {
   );
 }
 
-export { Completed }
+export { Done };
