6701df76dc26a4a0eea7673d8bc9f6225881cf2b
Deepak Chahal <chaliadeepakdaha@gmail.com>
2025-08-19 15:24:47 +0530

    done

--- day 13/backend-student-crud/server.js
+++ day 13/backend-student-crud/server.js
@@ -1,11 +1,21 @@
-
 // const express = require('express') 
 import express from 'express'
 
-const app = express()
-const port = 3000
+import connectToDatabase from './db.js';
+
+const app = express();
+const port = 3000;
 
 app.use(express.json())
+
+let db;
+
+// start server
+app.listen(port, async () => {
+    console.log(`my server is running at ${port}`)
+    db =  await connectToDatabase('students-db')
+
+})
 
 
 app.get('/test', (req, res) => {
@@ -15,56 +25,65 @@ app.get('/test', (req, res) => {
 
 
 // API 1 create student
-app.post('/insert-one-student', (req, res) => {
-    console.log(`request is ${JSON.stringify(req.body)}`)
-    res.send("student created successfully")
-})
-
-// API 2 Read a single student
-app.get('/read-one-student', (req, res) => {
-    console.log(`read student with id = ${req.query.id}`)
-    let studentObj = {
-        "id": 1,
-        "name": "ram",
-        "rollNo": 123
+app.post('/insert-one-student', async (req, res) => {
+    try {
+        let info = req.body;
+        console.log(`Request received: ${JSON.stringify(info)}`)
+        await db.collection('detail').insertOne(info)
+        res.status(201).json({ msg: "Student Created Successfully" })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
-    res.json(studentObj);
 })
 
-// API 3 read all students
-app.get('/read-many-student', (req, res) => {
-    console.log(`request is ${JSON.stringify(req.body)}`)
-    let studentArr = [
-        {
-            "id": 1,
-            "name": "ram",
-            "rollNo": 123
-        },
-        {
-            "id": 2,
-            "name": "shyam",
-            "rollNo": 456
-        }
-    ]
-    res.json(studentArr)
+// API 2 Read all students
+app.get('/read-many-student', async (req, res) => {
+    try {
+        let allInfo = await db.collection('detail').find().toArray()
+        res.status(200).json(allInfo)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+// API 3 read single student
+app.get('/read-single-student', async (req, res) => {
+    try {
+        let student = req.query.name;
+        let result = await db.collection('detail').findOne({ name: student })
+        if (!result) return res.status(404).json("No data found")
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 })
 
 // API 4 update student
-app.patch('/update-student', (req, res) => {
-    console.log(`user id = ${req.query.id}`)
-    console.log(`update data = ${req.body}`)
-    res.send("student updated successfully")
+app.patch('/update-student', async (req, res) => {
+    try {
+        let studentId = req.query.id;
+        let newInfo = req.body;
+        let result = await db.collection('detail')
+            .updateOne({ id: studentId }, { $set: newInfo }) // or use _id + ObjectId
+        if (result.matchedCount === 0) {
+            return res.status(404).json("Student not found")
+        }
+        res.status(200).json("Student updated successfully")
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 })
-
 
 // API 5 delete student
-app.delete('/delete-student', (req, res) => {
-    console.log(`delete user with id = ${req.query.id}`)
-    res.send("student deleted successfully")
-})
-
-
-// start server
-app.listen(port, () => {
-    console.log(`my server is running at ${port}`)
+app.delete('/delete-student', async (req, res) => {
+    try {
+        let name = req.query.name;
+        let result = await db.collection('detail').deleteOne({ name: name })
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ msg: 'Student not found' })
+        }
+        res.status(200).json({ msg: 'Student deleted successfully' })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 })
--- day 13/backend-todo-app/server.js
+++ day 13/backend-todo-app/server.js
@@ -1,15 +1,97 @@
-import express from 'express'
-const app = express()
-const port = 3000
 
-app.use(express.json())
-
-// API Section 
-
-// API 1 
+import express from 'express';
+import connectToDatabase from './db.js';
 
 
-// start server
-app.listen(port, () => {
-    console.log(`my server is running at ${port}`)
+const app = express();
+
+
+const port = 3000;
+
+app.use(express.json());
+
+let db;
+
+app.listen(port ,async ()=>{
+    console.log(`Server started at port ${[port]}`)
+    db = await connectToDatabase('todo-db')
+
+})
+
+
+
+app.post('/create-todo',async (request,response)=>{
+     try{
+            let task = request.body
+    console.log(`Request recieved at ${JSON.stringify(task)}`);
+    await db.Collection('tasks').insertOne(task)
+    response.status(201).json({
+     msg : "Task created successfully"
+    })
+     } catch(error){
+          response.status(500).json({
+               error : error.message
+          })
+     }
+   
+})
+
+
+
+app.get('/read-all-todo',(request,response)=>{
+   try{
+     let AllTasks = db.Collection('tasks').find()
+     response.status(200).json(AllTasks)
+   } catch(error){
+      response.status(500).json({
+               error : error.message
+          })
+    }
+
+})
+
+
+
+
+
+app.get('/read todo',async (request,response)=>{
+  try{
+  let Todo = req.query.id;
+  let result = await db.Collection('tasks').findOne(Todo)
+   if (result == null) {
+            res.status(404).json({ msg: 'Task not found' })
+        }
+        res.status(200).json(result);
+    }catch(error){
+     res.status(500).json({
+               error : error.message
+          })
+    }
+})
+
+
+
+app.patch('/update-todo',async (request,response)=>{
+
+     let Todo = req.query.id;
+     let newTodo = req.body
+     let result = await db.Collection('tasks').updateOne({id : Todo}, {$set : newTodo})
+      if (result.matchedCount == 0) {
+        return res.status(404).json({ msg: 'Task not found' });
+    }
+
+    res.status(201).json({ msg: 'Task updated successfully' });
+
+})
+
+
+
+
+app.delete('/delete-todo',async (request,response)=>{
+     let Todo = req.query.id;
+    let result = await db.collection('tasks').deleteOne({ id : Todo})
+    if (result.deletedCount == 0) {
+        res.status(404).json({ msg: 'Task not found' })
+    }
+    res.status(201).json({ msg: 'Task deleted successfully' })
 })
